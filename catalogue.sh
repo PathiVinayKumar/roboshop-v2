f5d3695d95148a7f9153aaf96c50499cb52c36cf
PathiVinayKumar <vinny.pathi@gmail.com>
2026-04-25 14:29:14 +0530

    latest changes for catalogue.sh

--- catalogue.sh
+++ catalogue.sh
@@ -4,54 +4,54 @@ echo Log file Output: ${log_file}
 
 
 echo -e "${hs} Install GoLang & MySQL ${he}" | tee -a ${log_file} 
-dnf install -y golang git mysql8.4
+dnf install -y golang git mysql8.4 &>>{log_file}
 echo $?
 
 echo -e "${hs} Copying the ${component_name} file ${he}" | tee -a ${log_file} 
-cp catalogue.service /etc/systemd/system/catalogue.service
-echo $?
+cp catalogue.service /etc/systemd/system/catalogue.service &>>{log_file}
+echo $? 
 
 echo -e "${hs} Downloading the file ${he}" | tee -a ${log_file} 
 curl -L -o /tmp/${component_name}.zip https://raw.githubusercontent.com/raghudevopsb89/roboshop-microservices/main/artifacts/catalogue.zip
 echo $?
 
 echo -e "${hs} Create folder for application ${he}" | tee -a ${log_file} 
-rm -rf /app
-mkdir -p /app
-cd /app
+rm -rf /app &>>{log_file}
+mkdir -p /app &>>{log_file}
+cd /app &>>{log_file}
 echo $?
 
 
-echo -e "${hs} Extract Application ${he}" | tee -a ${log_file} 
-unzip /tmp/${component_name}.zip
+echo -e "${hs} Extract Application ${he}" | tee -a ${log_file}  &>>{log_file}
+unzip /tmp/${component_name}.zip &>>{log_file}
 echo $?
 
 
-echo -e "${hs} Scehama Update ${he}" | tee -a ${log_file} 
-mysql -h mysql-dev.vinaykumar.online -u root -pRoboShop@1 < db/schema.sql
-mysql -h mysql-dev.vinaykumar.online -u root -pRoboShop@1 < db/app-user.sql
-mysql -h mysql-dev.vinaykumar.online -u root -pRoboShop@1 catalogue < db/master-data.sql
+echo -e "${hs} Scehama Update ${he}" | tee -a ${log_file}  &>>{log_file}
+mysql -h mysql-dev.vinaykumar.online -u root -pRoboShop@1 < db/schema.sql &>>{log_file}
+mysql -h mysql-dev.vinaykumar.online -u root -pRoboShop@1 < db/app-user.sql &>>{log_file}
+mysql -h mysql-dev.vinaykumar.online -u root -pRoboShop@1 catalogue < db/master-data.sql &>>{log_file}
 echo $?
 
 
 echo -e "${hs} Creating the application user ${he}" | tee -a ${log_file} 
-useradd -r -s /bin/false appuser
-cd /app
+useradd -r -s /bin/false appuser &>>{log_file}
+cd /app &>>{log_file}
 echo $?
 
 echo -e "${hs} Compile Application Code ${he}" | tee -a ${log_file} 
-go mod tidy
-CGO_ENABLED=0 go build -o /app/${component_name} .
+go mod tidy &>>{log_file}
+CGO_ENABLED=0 go build -o /app/${component_name} .  &>>{log_file}
 echo $?
 
 echo -e "${hs} Appying the permission for application folder ${he}" | tee -a ${log_file} 
-chown -R appuser:appuser /app
-chmod -R o-rwx /app
-echo $?
+chown -R appuser:appuser /app  &>>{log_file}
+chmod -R o-rwx /app  &>>{log_file}
+echo $? 
 
 
 echo -e "${hs} Reloading and Restarting ${he}" | tee -a ${log_file} 
-systemctl daemon-reload
+systemctl daemon-reload  &>>{log_file}
 systemctl enable ${component_name}
 systemctl restart ${component_name}
 echo $?
